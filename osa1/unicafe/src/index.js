45437dbd16a090319b944cff8b5133ffbc626805
katjasdf <katja.hinkkanen@outlook.com>
2020-05-14 00:35:21 +0300

    osa 1 unicafe done

---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = (good * 1 + neutral * 0 + bad * -1) / all
+  const positive = good / all * 100
+
+  if (all === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+
+    )
+  }
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text='good' value={good}/>
+          <StatisticLine text='neutral' value={neutral}/>
+          <StatisticLine text='bad' value={bad}/>
+          <StatisticLine text='all' value={all}/>
+          <StatisticLine text='average' value={average}/>
+          <StatisticLine text='positive' value={positive}/>
+        </tbody>
+      </table>
+    </div>
+  )
+    
+}
+
+const App = (props) => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodFeedback = () => setGood(good + 1)
+  const handleNeutralFeedback = () => setNeutral(neutral + 1)
+  const handleBadFeedback = () => setBad(bad + 1)
+
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={handleGoodFeedback} text='Good'/>
+        <Button handleClick={handleNeutralFeedback} text='Neutral'/>
+        <Button handleClick={handleBadFeedback} text='Bad'/>
+        <Statistics good={good} neutral={neutral} bad={bad}/>
+      </div>
+    )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
