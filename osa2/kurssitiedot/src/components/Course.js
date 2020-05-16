0d8c617d2cf28b119c3e4401ffbb8603f2aa5fe3
katjasdf <katja.hinkkanen@outlook.com>
2020-05-16 11:45:55 +0300

    osa 2 exercises 2.1-2.5 done

---
+++ osa2/kurssitiedot/src/components/Course.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Course = ({course}) => {
+    return (
+      <div>
+        <Header name={course.name}/>
+        <Content parts={course.parts}/>
+        <Total parts={course.parts}/>
+      </div>
+    )
+}
+
+const Header = ({name}) => {
+    return (
+      <div>
+        <h3>{name}</h3>
+      </div>
+    )
+}
+
+const Content = ({parts}) => {
+    const allParts = parts.map(part => <Part key={part.id} part={part}/>)
+
+    return (
+      <div>
+        {allParts}
+      </div>
+    )
+}
+
+const Part = ({part}) => {
+    return (
+      <div>
+        <p>{part.name} {part.exercises}</p>
+      </div>
+    )
+}
+
+const Total = ({parts}) => {
+    const total = parts.reduce((s, p) => s + p.exercises, 0)
+
+    return (
+      <div>
+        <p style={{fontWeight: 'bold'}}>Total of {total} exercises</p>
+      </div>
+    )
+}
+
+export default Course
